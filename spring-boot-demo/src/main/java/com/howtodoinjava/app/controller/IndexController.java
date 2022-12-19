aff5e0bbc585a76bb297151cd7c921c83170c244
Maksym Rohachov <romax982@gmail.com>
2022-12-19 12:31:54 +0100

    Update the logic of the project

--- spring-boot-demo/src/main/java/com/howtodoinjava/app/controller/IndexController.java
+++ spring-boot-demo/src/main/java/com/howtodoinjava/app/controller/IndexController.java
@@ -2,10 +2,10 @@ package com.howtodoinjava.app.controller;
 
 import java.util.Map;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
 	@RequestMapping("/")
